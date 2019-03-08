8ac0db92fa28cf2870970a062946244507feb809
lixas <lixas666@gmail.com>
2019-03-08 10:43:40 +0000

    Revert "Added simple spinner (dropdown) component"
    This reverts commit 51b648245ebb8eaa7a0e36c2806a6f7e5a6f5a10.

--- app/src/main/java/com/jasonette/seed/Component/JasonComponentFactory.java
+++ app/src/main/java/com/jasonette/seed/Component/JasonComponentFactory.java
@@ -40,8 +40,6 @@ public class JasonComponentFactory {
                 view = JasonSliderComponent.build(prototype, component, parent, context);
             } else if(type.equalsIgnoreCase("switch")) {
                 view = JasonSwitchComponent.build(prototype, component, parent, context);
-            } else if(type.equalsIgnoreCase("spinner")) {
-                view = JasonSpinnerComponent.build(prototype, component, parent, context);
             } else {
                 // Non-existent component warning
                 JSONObject error_component = new JSONObject(component.toString());
